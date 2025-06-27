3cd7b474857557ea2c8dc5634352b96f9f49772f
yoris29 <ydoumi29@gmail.com>
2025-06-27 16:34:11 +0100

    Same col and row playfair cipher done

--- playfair.py
+++ playfair.py
@@ -21,7 +21,7 @@ for letter in alphabet:
     elif letter not in matrix:
         matrix.append(letter)
 
-matrix = np.matrix(matrix).reshape(5, 5)
+matrix = np.array(matrix).reshape(5, 5)
 
 plain = plain.replace(" ", "")
 plain_pairs = []  # he lx lo
@@ -29,10 +29,48 @@ i = 0
 
 while (i < len(plain) - 1):
     if plain[i] == plain[i+1]:
-       plain_pairs.append([plain[i], 'x']) 
+       plain_pairs.append(plain[i] + 'x') 
        i += 1
     else:
-        plain_pairs.append([plain[i], plain[i+1]])
-        i += 1
+        plain_pairs.append(plain[i] + plain[i+1])
+        i += 2
+# In case the last letter is alone
+if(len(plain_pairs) * 2 <len(plain)):
+    plain_pairs.append(plain[- 1]+'x')
 
+# encyption
+cipher = ""
+for pair in plain_pairs:
+    pair_handled = False
+    # letters in same row
+    for row in range(0,5):
+        current_row = matrix[row, :]
+        if(pair[0] in current_row and pair[1] in current_row):
+            first_index = list(current_row).index(pair[0])
+            second_index = list(current_row).index(pair[1])
+            cipher += matrix[row, (first_index+1) % 5]
+            cipher += matrix[row, (second_index+1) % 5]
+            pair_handled = True
+            break
+
+    if pair_handled:
+        continue
+
+    # letters in the same column
+    for col in range(0,5):
+        current_col = matrix[:, col]
+        if(pair[0] in current_col and pair[1] in current_col):
+            first_index = list(current_col).index(pair[0])
+            second_index = list(current_col).index(pair[1])
+            cipher += matrix[(first_index+1) % 5, col]
+            cipher += matrix[(second_index+1) % 5, col]
+            pair_handled = True
+            break
+
+    if pair_handled:
+        continue
+
+    
 print(matrix)
+
+## letters in same col
